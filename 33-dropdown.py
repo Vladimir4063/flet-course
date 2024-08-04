5281dbed54e26ee2a16a1fdda71e2312870078de
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2024-08-04 13:36:20 -0300

    Dropdown

---
+++ 33-dropdown.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+def main(page: ft.Page):
+    def button_clicked(e):
+        txt_output.value = f"Selecionaste el color: {color_dropdown.value}"
+        page.update()
+
+    txt_output = ft.Text()
+    btn_submit = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+    color_dropdown = ft.Dropdown(
+        width=100,
+        options=[
+            ft.dropdown.Option("Red"),
+            ft.dropdown.Option("Green"),
+            ft.dropdown.Option("Blue")
+        ]
+    )
+
+    page.add(
+        color_dropdown,
+        btn_submit,
+        txt_output
+    )
+
+ft.app(main)
