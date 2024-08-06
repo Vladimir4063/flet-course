cc486d44efeaf3b61f66a4f25ffb87051d636abb
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2024-08-06 15:42:46 -0300

    add gridView custom

--- 36-gridView.py
+++ 36-gridView.py
@@ -11,7 +11,7 @@ def main(page:ft.Page):
         expand=1,
         runs_count=5,
         max_extent=150,
-        child_aspect_ratio=1.0,
+        child_aspect_ratio=0.5,
         spacing=5,
         run_spacing=5,
     )
@@ -21,11 +21,15 @@ def main(page:ft.Page):
     # Inserto mediante "append" imagenes dentro de cada objeto de "images" y las renderizo
     for i in range(0,60):
         images.controls.append(
-            ft.Image(
-                src=f"https://picsum.photos/150/150?{i}",
-                fit=ft.ImageFit.NONE,
-                repeat=ft.ImageRepeat.NO_REPEAT,
-                border_radius=ft.border_radius.all(10),
+            ft.Container(
+                ft.Image(
+                    src=f"https://picsum.photos/150/150?{i}",
+                    fit=ft.ImageFit.NONE,
+                    repeat=ft.ImageRepeat.NO_REPEAT,
+                    border_radius=ft.border_radius.all(10),
+                ),
+                # marco un color para ver el cambio efectuado en child_aspect_ratio=1.5
+                bgcolor=ft.colors.AMBER
             )
         )
     page.update()
