19ec40baa112e560d4babb7d7632125454003f6c
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2024-11-04 17:30:43 -0300

    update app manadeldia

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import requests
 
 
 def main(page: ft.Page):
@@ -7,11 +8,40 @@ def main(page: ft.Page):
     page.window.height = 810
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    NRO_PARASHA = 47
-    NAME_PARASHA = "Reé"
-    SIGN_PARASHA = "OBSERVA"
-    INFO_ALIYA = "Aliyá 1, Deuteronomio 11:26 - 12:10"
-    ALIYA = ("""26 Miren, hoy les doy a elegir entre la bendición y la maldición: 27 bendición, si obedecen los mandamientos que yo, el Señor su Dios, hoy les mando obedecer; 28 maldición, si desobedecen los mandamientos del Señor su Dios y se apartan del camino que hoy les mando seguir, y se van tras dioses extraños que jamás han conocido. 29 Cuando el Señor su Dios los haya hecho entrar en la tierra que van a poseer, ustedes bendecirán al monte Guerizín y maldecirán al monte Ebal. 30 Esos montes están al otro lado del Jordán, hacia el oeste, en el territorio de los cananeos que viven en el Arabá, en la vecindad de Guilgal, junto a las encinas de Moré. 31 Ustedes están a punto de cruzar el Jordán y entrar a tomar posesión de la tierra que les da el Señor su Dios. Cuando la hayan tomado y ya estén viviendo allí, 32 cuiden de obedecer todos los estatutos y las leyes que hoy les mando.""" * 4)
+    nro_parasha = None
+    name_parasha = None
+    sign_parasha = None
+    info_aliya = None
+    aliya = None
+
+    def fetch_api_data():
+        data = None
+        nonlocal nro_parasha, name_parasha, sign_parasha, info_aliya, aliya
+        try:
+            response = requests.get("https://manadeldia.vercel.app/api/get/aliya")
+            if response.status_code == 200:
+                print(data)
+                data = response.json()
+                # Asignar los datos a las variables
+                nro_parasha = data['nroParasha']
+                name_parasha = data['nameParasha']
+                sign_parasha = data['signParasha']
+                info_aliya = f"Aliyá 1, {data['section1']}"
+                aliya = data['aliya1']
+            else:
+                print("Error al obtener datos de la API")
+        except Exception as e:
+            print(f"Excepción al conectar con la API: {e}")
+
+    nro_parasha = 47
+    name_parasha = "Reé"
+    sign_parasha = "OBSERVA"
+    info_aliya = "Aliyá 1, Deuteronomio 11:26 - 12:10"
+    aliya = ("""26 Miren, hoy les doy a elegir entre la bendición y la maldición: 27 bendición, si obedecen los mandamientos que yo, el Señor su Dios, hoy les mando obedecer; 28 maldición, si desobedecen los mandamientos del Señor su Dios y se apartan del camino que hoy les mando seguir, y se van tras dioses extraños que jamás han conocido. 29 Cuando el Señor su Dios los haya hecho entrar en la tierra que van a poseer, ustedes bendecirán al monte Guerizín y maldecirán al monte Ebal. 30 Esos montes están al otro lado del Jordán, hacia el oeste, en el territorio de los cananeos que viven en el Arabá, en la vecindad de Guilgal, junto a las encinas de Moré. 31 Ustedes están a punto de cruzar el Jordán y entrar a tomar posesión de la tierra que les da el Señor su Dios. Cuando la hayan tomado y ya estén viviendo allí, 32 cuiden de obedecer todos los estatutos y las leyes que hoy les mando.""" * 4)
+
+    # call api
+    # fetch_api_data()
+
 
     content = ft.Column(scroll=ft.ScrollMode.AUTO, expand=True)
 
@@ -62,9 +92,9 @@ def main(page: ft.Page):
             # IMAGE
             img_moises = ft.Container(
                 content=ft.Image(
-                    src=f"assets/moises.png",
-                    width=250,
-                    height=250,
+                    src="https://manadeldia.vercel.app/static/img/moises.png",
+                    width=200,
+                    height=200,
                     fit=ft.ImageFit.CONTAIN,
                 ),
                 alignment=ft.alignment.center
@@ -72,7 +102,7 @@ def main(page: ft.Page):
 
             parasha_nro_txt = ft.Container(
                 content=ft.Text(
-                    f"PARASHA {NRO_PARASHA}",
+                    f"PARASHA {nro_parasha}",
                     theme_style=ft.TextThemeStyle.BODY_SMALL,
                 ),
                 alignment=ft.alignment.center
@@ -80,7 +110,7 @@ def main(page: ft.Page):
 
             name_parasha_txt = ft.Container(
                 content=ft.Text(
-                    f"{NAME_PARASHA}",
+                    f"{name_parasha}",
                     size=40,
                     weight=ft.FontWeight.W_600,
                     italic=True
@@ -90,7 +120,7 @@ def main(page: ft.Page):
 
             sig_parasha_txt = ft.Container(
                 content=ft.Text(
-                    f"-{SIGN_PARASHA}-",
+                    f"{sign_parasha}",
                     size=50,
                     weight=ft.FontWeight.W_700
                 ),
@@ -99,7 +129,7 @@ def main(page: ft.Page):
 
             info_parasha_txt = ft.Container(
                 content=ft.Text(
-                    f"{INFO_ALIYA}",
+                    f"{info_aliya}",
                     size=13,
                     italic=True,
                     color=ft.colors.BLACK87
@@ -108,7 +138,7 @@ def main(page: ft.Page):
             )
 
             aliya_txt = ft.Container(
-                content=ft.Text(ALIYA),
+                content=ft.Text(aliya),
                 padding=10,
                 bgcolor=ft.colors.AMBER_50,
                 border_radius=10
